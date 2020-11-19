adf60b0ca52f0e77148459b1fa2615f0499a8c59
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-11-19 18:08:18 -0300

    Criando classe ConnectioFactory responsável pela conexão com o banco de dados

---
+++ src/br/com/gestao/eventos/factory/ConnectionFactory.java
@@ -0,0 +1,27 @@
+package br.com.gestao.eventos.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	//Nome do Usuário MySQL
+	private static final String USERNAME = "root";
+
+	//Senha do Banco de dados
+	private static final String PASSWORD = "admin123456";
+
+	//Caminho do banco de dados, porta, nome do Banco
+	private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/agenda?useTimezone=true&serverTimezone=UTC&useSSL=false";
+
+	//Conexão com o banco de dados
+	public static Connection createConnectionToMySQL() throws SQLException, ClassNotFoundException {
+		//Faz com que a classe seja carregada pela JVM
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		//Cria conexão com o banco de dados
+		Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+		return connection;
+	}
+	
+}
