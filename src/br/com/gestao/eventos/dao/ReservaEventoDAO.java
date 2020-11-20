5f51b567af56aa2b4edfd5d936d2d7164aed89b6
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-11-20 01:06:25 -0300

    criando classe reservaevento e com método inserir

--- src/br/com/gestao/eventos/dao/ReservaEventoDAO.java
+++ src/br/com/gestao/eventos/dao/ReservaEventoDAO.java
@@ -1,5 +1,128 @@
 package br.com.gestao.eventos.dao;
 
-public class ReservaEventoDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
+import br.com.gestao.eventos.factory.ConnectionFactory;
+import br.com.gestao.eventos.model.ReservaEvento;
+
+public class ReservaEventoDAO {
+	public void save(ReservaEvento reservaEvento) {
+		String sql = "INSERT INTO reservaevento (nomeEvento, horarioEvento, dataEvento, descricaoEvento, idEspaco, idProfessor) VALUES(?,?,?,?,?,?)";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		try {
+			conn = ConnectionFactory.createConnectionToMySQL();
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+			
+			pstm.setString(1, reservaEvento.getNomeEvento());
+			pstm.setString(2, reservaEvento.getHorarioEvento());
+			pstm.setString(3, reservaEvento.getDataEvento());
+			pstm.setString(4, reservaEvento.getDescricaoEvento());
+			pstm.setInt(5, reservaEvento.getIdEspaco());			
+			pstm.setInt(6, reservaEvento.getIdProfessor());
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			//Fechar as conexões
+			try{
+				if(pstm!=null) {
+					pstm.close();
+				}
+				
+				if(conn!=null) {
+					conn.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public List<> read(){
+		String sql = "";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		try {
+			conn = ConnectionFactory.createConnectionToMySQL();
+			
+			// 
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			//Fechar as conexões
+			try{
+				if(pstm!=null) {
+					pstm.close();
+				}
+				
+				if(conn!=null) {
+					conn.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void update(ReservaEvento reservaEvento) {
+		String sql = "";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		try {
+			conn = ConnectionFactory.createConnectionToMySQL();
+			
+			// 
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			//Fechar as conexões
+			try{
+				if(pstm!=null) {
+					pstm.close();
+				}
+				
+				if(conn!=null) {
+					conn.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void delete(ReservaEvento reservaEvento) {
+		String sql = "";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		try {
+			conn = ConnectionFactory.createConnectionToMySQL();
+			
+			// 
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			//Fechar as conexões
+			try{
+				if(pstm!=null) {
+					pstm.close();
+				}
+				
+				if(conn!=null) {
+					conn.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
 }
--- src/br/com/gestao/eventos/model/ReservaEvento.java
+++ src/br/com/gestao/eventos/model/ReservaEvento.java
@@ -1,10 +1,32 @@
 package br.com.gestao.eventos.model;
 
 public class ReservaEvento {
+	private static int idReservaEvento;
 	private static String nomeEvento;
 	private static String horarioEvento;
 	private static String dataEvento;
 	private static String descricaoEvento;
+	private static int idEspaco;
+	private static int idProfessor;
+	
+	public static int getIdReservaEvento() {
+		return idReservaEvento;
+	}
+	public static void setIdReservaEvento(int idReservaEvento) {
+		ReservaEvento.idReservaEvento = idReservaEvento;
+	}
+	public static int getIdEspaco() {
+		return idEspaco;
+	}
+	public static void setIdEspaco(int idEspaco) {
+		ReservaEvento.idEspaco = idEspaco;
+	}
+	public static int getIdProfessor() {
+		return idProfessor;
+	}
+	public static void setIdProfessor(int idProfessor) {
+		ReservaEvento.idProfessor = idProfessor;
+	}
 	
 	public static String getNomeEvento() {
 		return nomeEvento;
