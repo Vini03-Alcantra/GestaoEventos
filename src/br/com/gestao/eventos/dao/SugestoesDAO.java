871201f92b243c05db4fba7cb8a48dd89235c46b
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-11-20 00:37:25 -0300

    iniciando sugestoes com insert

--- src/br/com/gestao/eventos/dao/SugestoesDAO.java
+++ src/br/com/gestao/eventos/dao/SugestoesDAO.java
@@ -1,5 +1,129 @@
 package br.com.gestao.eventos.dao;
 
-public class SugestoesDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.List;
 
+import br.com.gestao.eventos.factory.ConnectionFactory;
+import br.com.gestao.eventos.model.Sugestoes;
+
+public class SugestoesDAO {
+	public void save(Sugestoes sugestoes) {
+		String sql = "INSERT INTO sugestoes(emailEnviado, nomeEnviado, assunto) VALUES (?, ?, ?)";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		try {
+			conn = ConnectionFactory.createConnectionToMySQL();
+			
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+			
+			pstm.setString(1, sugestoes.getEmailEnviado());
+			pstm.setString(2, sugestoes.getNomeEnviado());
+			pstm.setString(3, sugestoes.getAssunto());
+			
+			pstm.execute();
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			//Fechar as conexões
+			try{
+				if(pstm!=null) {
+					pstm.close();
+				}
+				
+				if(conn!=null) {
+					conn.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public List<Sugestoes> read(){
+		String sql = "";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		try {
+			conn = ConnectionFactory.createConnectionToMySQL();
+			
+			// 
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			//Fechar as conexões
+			try{
+				if(pstm!=null) {
+					pstm.close();
+				}
+				
+				if(conn!=null) {
+					conn.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void update(Sugestoes sugestoes) {
+		String sql = "";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		try {
+			conn = ConnectionFactory.createConnectionToMySQL();
+			
+			// 
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			//Fechar as conexões
+			try{
+				if(pstm!=null) {
+					pstm.close();
+				}
+				
+				if(conn!=null) {
+					conn.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void delete(Sugestoes sugestoes) {
+		String sql = "";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		try {
+			conn = ConnectionFactory.createConnectionToMySQL();
+			
+			// 
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			//Fechar as conexões
+			try{
+				if(pstm!=null) {
+					pstm.close();
+				}
+				
+				if(conn!=null) {
+					conn.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
 }
