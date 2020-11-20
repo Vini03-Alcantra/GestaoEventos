261e24ce6c7d5d6cd6ccf537b9e379aa01a0a9ee
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-11-20 01:15:33 -0300

    criando classe reservaevento e com método read

--- src/br/com/gestao/eventos/dao/ReservaEventoDAO.java
+++ src/br/com/gestao/eventos/dao/ReservaEventoDAO.java
@@ -2,6 +2,9 @@ package br.com.gestao.eventos.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.gestao.eventos.factory.ConnectionFactory;
 import br.com.gestao.eventos.model.ReservaEvento;
@@ -40,32 +43,51 @@ public class ReservaEventoDAO {
 		}
 	}
 	
-	public List<> read(){
-		String sql = "";
+	public List<ReservaEvento> read(){
+		String sql = "SELECT * FROM reservaevento";
+		
+		List<ReservaEvento> reservaEvento = new ArrayList<ReservaEvento>();
 		
 		Connection conn = null;
 		PreparedStatement pstm = null;
+		ResultSet rset = null;
 		try {
 			conn = ConnectionFactory.createConnectionToMySQL();
-			
-			// 
 			pstm = (PreparedStatement) conn.prepareStatement(sql);
+			rset = pstm.executeQuery();
+			
+			while(rset.next()) {
+				ReservaEvento reservas = new ReservaEvento();
+				reservas.setIdReservaEvento(rset.getInt("idReservaEvento"));
+				reservas.setNomeEvento(rset.getString("nomeEvento"));
+				reservas.setHorarioEvento(rset.getString("horarioEvento"));
+				reservas.setDataEvento(rset.getString("dataEvento"));
+				reservas.setDescricaoEvento(rset.getString("descricaoEvento"));
+				reservas.setIdEspaco(rset.getInt("idEspaco"));
+				reservas.setIdProfessor(rset.getInt("idProfessor"));
+				
+				reservaEvento.add(reservas);
+			}
 		} catch(Exception e) {
 			e.printStackTrace();
 		} finally {
-			//Fechar as conexões
-			try{
-				if(pstm!=null) {
-					pstm.close();
+			try {
+				if(rset!=null) {
+					rset.close();
 				}
 				
 				if(conn!=null) {
-					conn.close();
+					rset.close();
+				}
+				
+				if(pstm != null) {
+					pstm.close();
 				}
 			} catch(Exception e) {
 				e.printStackTrace();
 			}
 		}
+		return reservaEvento;		
 	}
 	
 	public void update(ReservaEvento reservaEvento) {
