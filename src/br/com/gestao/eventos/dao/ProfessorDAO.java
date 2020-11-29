df429317b73a76eb64de235c2879860d7299e09c
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-11-28 21:15:30 -0300

    método save professor criado!

--- src/br/com/gestao/eventos/dao/ProfessorDAO.java
+++ src/br/com/gestao/eventos/dao/ProfessorDAO.java
@@ -2,7 +2,10 @@ package br.com.gestao.eventos.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.gestao.eventos.factory.ConnectionFactory;
 import br.com.gestao.eventos.model.Professor;
@@ -35,5 +38,48 @@ public class ProfessorDAO {
 				pstm.close();
 			}			
 		}
+	}
+	
+	public List<Professor> read() throws SQLException{
+		String sql = "SELECT * FROM professor";
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		ResultSet rset = null;
+		List<Professor> professores = new ArrayList<Professor>();
+				
+		try {
+			conn = ConnectionFactory.createConnectionToMySQL();
+			pstm = conn.prepareStatement(sql);
+			rset = pstm.executeQuery();
+			
+			while(rset.next()) {
+				Professor professor = new Professor();
+				
+				professor.setIdProfessor(rset.getInt("idProfessor"));
+				professor.setNomeProfessor(rset.getString("nomeProfessor"));
+				professor.setMatriculaProfessor(rset.getInt("matriculaProfessor"));
+				professor.setCursoProfessorVinculado(rset.getString("cursoProfessorVinculado"));
+				professor.setLoginProfessor(rset.getString("loginProfessor"));  
+				professor.setEmailProfessor(rset.getString("senhaProfessor"));  
+				professor.setSenhaPorfessor(rset.getString("emailProfessor"));
+				
+				professores.add(professor);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rset!=null) {
+				rset.close();
+			}
+			if(pstm!=null) {
+				pstm.close();
+			}
+			
+			if(conn!=null) {
+				conn.close();
+			}
+		}
+		
+		return professores;
 	}
 }
